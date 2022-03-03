abcad222ee7a984924276ad15d69a530c58c9ae7
Sylvan Martin <alephnulltim@gmail.com>
2022-03-03 18:15:49 -0500

    Began some point addition

--- src/elliptic_curve.ml
+++ src/elliptic_curve.ml
@@ -1,3 +1,4 @@
+open Z
 
 type field = {
   p : Z.t ; (* The size of the field *)
@@ -15,6 +16,8 @@ type point = {
 
 exception InvalidPoint of point
 
+(** EXPOSED **)
+
 let add_points f p1 p2 =
   raise (Failure "Unimplemented: from_file")
 (** [add_points] adds p1 and p2 in field f. Requires: [f] *)
@@ -22,3 +25,27 @@ let add_points f p1 p2 =
 let multiply_point f n p = 
   raise (Failure "Unimplemented: from_file")
 (** [multiply_point] adds a point with itself n times *)
+
+
+(** HIDDEN **)
+
+let lambda_intersection l p1 p2 = 
+  let x = (Z.pow l 2) - p1.x - p2.x in 
+  let y = l * (p1.x - x) - p1.y in 
+  { x = x ; y = y }
+
+(* Adds two points using standard calculation, no tricks used *)
+let simple_add p1 p2 = 
+  let lambda = (p2.y - p1.y) / (p2.x - p2.x) in 
+  lambda_intersection lambda p1 p2
+
+(* Doubles a point by adding it to itself, 
+  using the tangent line to the curve *)
+let double f p =
+  let lambda = ((3 |> Z.of_int) * (Z.pow p.x 2) + f.a) 
+    / ((2 |> Z.of_int) * p.y) in 
+  lambda_intersection lambda p p (* TODO - Maybe make this more efficient? *)
+
+(* Negates a point *)
+let negate p = 
+  { p with y = -p.y }
