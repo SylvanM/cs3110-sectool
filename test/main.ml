ef9822e84364d9942ea3c769fa0b474319a24963
Sylvan Martin <alephnulltim@gmail.com>
2022-03-20 15:55:20 -0400

    Re-added ECC test cases

--- test/main.ml
+++ test/main.ml
@@ -35,10 +35,6 @@ let double_test name f p dp =
   let dub = multiply_point f (Z.of_int 2) p in 
   name >:: fun _ -> assert_equal dp dub ~printer:string_of_point
 
-let modulus_test name f expected_m = 
-  let m = f |> get_modulus in 
-  name >:: fun _ -> assert_equal expected_m m ~printer:Z.to_string
-
 let fio_tests = [
   
 ]
@@ -88,7 +84,11 @@ let fio_tests = [
 ]
 
 let ecc_tests = [
-  
+  (* same_secret_test "Simple Curve 1 and 2" simple_curve Z.one (2 |> Z.of_int) ; *)
+  (* same_secret_test "25519 1 and 2" curve25519 Z.one (2 |> Z.of_int) ; *)
+
+  add_test "(1, 6) + (4, 6)" example_curve (make_int_point (1, 6)) (make_int_point (4, 6)) (make_int_point (8, 7));
+  double_test "2 * (1, 6)" example_curve (make_int_point (1, 6)) (make_int_point (10, 1));
 ]
 
 let aes_tests = [
