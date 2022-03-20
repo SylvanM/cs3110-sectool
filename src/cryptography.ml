3f658c90f60dc01cb700e8ef1ad5aa66941d15f3
Heewon Ahn <ha268@cornell.edu>
2022-03-20 15:01:27 -0400

    update

--- src/cryptography.ml
+++ src/cryptography.ml
@@ -2,4 +2,4 @@ open Elliptic_curve
 
 let generate_secret f n p =
     let point = multiply_point f n p in
-        {x=point.x mod f.p ; y=point.x mod f.p}
+        {x=point.x mod f.p ; y=point.x mod f.p}g
--- src/file_wizard.ml
+++ src/file_wizard.ml
@@ -20,31 +20,34 @@ let rec to_int (l : string list) : int list =
 let read_private_key (f : string) : int =
   let list = Stdio.In_channel.read_lines (dir_prefix ^ f) in
     match list with
+    | exception e -> raise (FileDoesNotExist (f ^ " does not exist"))
     | h::t -> int_of_string h
-    | _ -> raise (Malformed "Error")
+    | _ -> raise (Malformed "Incorrectly Formatted")
 
 let read_public_key (f : string) : Elliptic_curve.point =
   let list = Stdio.In_channel.read_lines (dir_prefix ^ f) in
     match list with
-    | [] -> raise (Malformed "Error")
+    | exception e -> raise (FileDoesNotExist (f ^ " does not exist"))
+    | [] -> raise (Malformed "Empty File")
     | h::t ->
       let str_list = String.split_on_char ' ' h in
         let int_list = to_int str_list in
           match int_list with
           | x::y::t -> { x=x; y=y}
-          | _::[] | [] -> raise (Malformed "Error")
+          | _::[] | [] -> raise (Malformed "Empty File")
 
 let read_domain_params (f : string) : Elliptic_curve.field =
   let list = Stdio.In_channel.read_lines (dir_prefix ^ f) in
     match list with
-    | [] -> raise (Malformed "Error")
+    | exception e -> raise (FileDoesNotExist (f ^ " does not exist"))
+    | [] -> raise (Malformed "Empty File")
     | h::t ->
       let str_list = String.split_on_char ' ' h in
         let int_list = to_int str_list in
           match int_list with
           | p::a::b::c::d::g::n::h::t -> create_field (p,a,b,c,d,g,n,h)
-          | _::[] | [] -> raise (Malformed "Error")
-          | h::t -> raise (Malformed "Error")
+          | _::[] | [] -> raise (Malformed "Empty File")
+          | h::t -> raise (Malformed "Incorrectly Formatted")
 
 let write_private_key (d : int) (f : string) =
   Stdio.Out_channel.write_lines (dir_prefix ^ f) [string_of_int d]
