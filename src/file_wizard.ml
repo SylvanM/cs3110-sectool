f675904d72928cc4842cba81564695b8a7b414d7
Heewon Ahn <ha268@cornell.edu>
2022-05-18 12:57:29 -0400

    e

--- src/file_wizard.ml
+++ src/file_wizard.ml
@@ -30,12 +30,21 @@ let read_private_key (f : string) : Z.t =
 
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 let read_public_key (f : string) : ED25519.point =
   let str_list = read_file f in
   let int_list = strs_to_z str_list in
     match int_list with
     | x::y::t -> make_point (x, y)
     | _::[] | [] -> raise (Malformed "Empty File")
+=======
+let read_public_key (f : string) : point =
+  let str_list = read_file f in
+  let int_list = strs_to_z str_list in
+    match int_list with
+    | x :: y :: t -> make_point (x,y)
+    | _ -> raise (Malformed "Empty File")
+>>>>>>> Stashed changes
 =======
 let read_public_key (f : string) : point =
   let str_list = read_file f in
@@ -59,7 +68,11 @@ let write_private_key (d : Z.t) (f : string) =
 let write_public_key (p : point) (f : string) =
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   Stdio.Out_channel.write_lines (dir_prefix ^ f) [string_of_point p]
+=======
+  Stdio.Out_channel.write_lines (dir_prefix ^ f) [p |> string_of_point]
+>>>>>>> Stashed changes
 =======
   Stdio.Out_channel.write_lines (dir_prefix ^ f) [p |> string_of_point]
 >>>>>>> Stashed changes
