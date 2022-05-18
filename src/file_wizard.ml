4ca8d68456b85031f81e5b5d615a209abeb60c10
Heewon Ahn <ha268@cornell.edu>
2022-05-18 13:13:52 -0400

    r

--- src/file_wizard.ml
+++ src/file_wizard.ml
@@ -28,67 +28,15 @@ let read_private_key (f : string) : Z.t =
     | h::t -> Z.of_string h
     | _ -> raise (Malformed "Incorrectly Formatted")
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 let read_public_key (f : string) : ED25519.point =
   let str_list = read_file f in
   let int_list = strs_to_z str_list in
     match int_list with
     | x::y::t -> make_point (x, y)
     | _::[] | [] -> raise (Malformed "Empty File")
-=======
-let read_public_key (f : string) : point =
-  let str_list = read_file f in
-  let int_list = strs_to_z str_list in
-    match int_list with
-    | x :: y :: t -> make_point (x,y)
-    | _ -> raise (Malformed "Empty File")
->>>>>>> Stashed changes
-=======
-let read_public_key (f : string) : point =
-  let str_list = read_file f in
-  let int_list = strs_to_z str_list in
-    match int_list with
-    | x :: y :: t -> make_point (x,y)
-    | _ -> raise (Malformed "Empty File")
->>>>>>> Stashed changes
-=======
-let read_public_key (f : string) : point =
-  let str_list = read_file f in
-  let int_list = strs_to_z str_list in
-    match int_list with
-    | x :: y :: t -> make_point (x,y)
-    | _ -> raise (Malformed "Empty File")
->>>>>>> Stashed changes
-=======
-let read_public_key (f : string) : point =
-  let str_list = read_file f in
-  let int_list = strs_to_z str_list in
-    match int_list with
-    | x :: y :: t -> make_point (x,y)
-    | _ -> raise (Malformed "Empty File")
->>>>>>> Stashed changes
 
 let write_private_key (d : Z.t) (f : string) =
   Stdio.Out_channel.write_lines (dir_prefix ^ f) [d |> Z.to_string]
 
 let write_public_key (p : point) (f : string) =
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  Stdio.Out_channel.write_lines (dir_prefix ^ f) [string_of_point p]
-=======
   Stdio.Out_channel.write_lines (dir_prefix ^ f) [p |> string_of_point]
->>>>>>> Stashed changes
-=======
-  Stdio.Out_channel.write_lines (dir_prefix ^ f) [p |> string_of_point]
->>>>>>> Stashed changes
-=======
-  Stdio.Out_channel.write_lines (dir_prefix ^ f) [p |> string_of_point]
->>>>>>> Stashed changes
-=======
-  Stdio.Out_channel.write_lines (dir_prefix ^ f) [p |> string_of_point]
->>>>>>> Stashed changes
