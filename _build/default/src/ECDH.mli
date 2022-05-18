5063d2775c2569d308fdc8ff935afa722bc66132
Heewon Ahn <ha268@cornell.edu>
2022-05-18 13:03:11 -0400

    finish up

--- _build/default/src/ECDH.mli
+++ _build/default/src/ECDH.mli
@@ -11,8 +11,7 @@ val generate_private_key : unit -> Z.t
 val compute_public_key : Z.t -> point
 (** [compute_public_key f d] computes the public key from a private key d *)
 
-(* val compute_shared_secret : Z.t -> Z.t -> Z.t *)
-val compute_shared_secret : Z.t -> point -> Z.t
+val compute_shared_secret : Z.t -> point -> point
 (** [compute_shared_secret f d p] uses this party's private key and the other
 	party's pubic key p to generate shared secret *)
 
